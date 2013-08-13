7343d553d0491a423205c1fe4f8b7044b459962d
jason perez <jperezish@gmail.com>
2013-08-13 16:49:27 -0700

    Add a static Contact Us page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -38,4 +38,16 @@ describe "Static pages" do
     end
   end
 
+  describe "Contact page" do
+    it "should have the content 'Contact Us'" do
+      visit "/static_pages/contact"
+      expect(page).to have_content("Contact Us")
+    end
+
+    it "should have the right title" do
+      visit "/static_pages/contact"
+      expect(page).to have_title("Contact Us | Ruby on Rails Tutorial Sample App")
+    end
+  end
+
 end
